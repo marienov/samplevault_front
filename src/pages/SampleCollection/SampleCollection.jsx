36351f990d9949b63d7c39e9e5b1f3680ca0b255
marienov <senya2104@gmail.com>
2024-03-16 17:38:43 +0300

    one more fake button

--- src/pages/SampleCollection/SampleCollection.jsx
+++ src/pages/SampleCollection/SampleCollection.jsx
@@ -3,11 +3,18 @@ import "./SampleCollection.css"
 import Body1 from "./UI/body/body1"
 
 const SampleCollection = () => {
+    const handleFileUpload = (event) => {
+        const file = event.target.files[0];
+        console.log('Выбранный файл:', file);
+    };
     return (
-            <div className="right-selection">
-                Я страница с коллекцией сэмплов
-            </div>,
+        <div >
+            <input type="file" onChange={handleFileUpload} id="fileInput" className="custom-file-input" />
+            <label htmlFor="fileInput" className="custom-file-label">
+            </label>
+        
             <Body1 />
+        </div>
     );
 };
 
