5583322735a4b5de9449785a5fde3163822533ad
angelova <angelova.03@mail.ru>
2024-03-16 14:25:37 +0300

    one laaast time

--- src/pages/SampleGen/SampleGen.jsx
+++ src/pages/SampleGen/SampleGen.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./SampleGen.css"
 
+import Body from "./UI/body/body"
+
 const SampleGen = () => {
     return (
             <div className="right-selection">
-                Я страница с генерацией
+                <Body />
             </div>
     );
 };
