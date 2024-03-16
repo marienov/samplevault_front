b28cac1ea79ca315a67d99228c28c42f0ce4a70f
marienov <senya2104@gmail.com>
2024-03-16 17:53:33 +0300

    file sender

--- src/pages/SampleCollection/SampleCollection.jsx
+++ src/pages/SampleCollection/SampleCollection.jsx
@@ -2,10 +2,30 @@ import React from 'react';
 import "./SampleCollection.css"
 import Body1 from "./UI/body/body1"
 
-const SampleCollection = () => {
-    const handleFileUpload = (event) => {
+const SampleCollection = () =>  {
+    const handleFileUpload = async (event) => {
         const file = event.target.files[0];
         console.log('Выбранный файл:', file);
+
+        const formData = new FormData();
+        formData.append('audio', file);
+
+        try {
+            const response = await fetch('https://samplevault.ru/api/v1/samples/upload', {
+                method: 'POST',
+                body: formData,
+                mode: 'no-cors'
+            });
+            console.log(response)
+            if (!response) {
+                throw new Error('Ошибка при загрузке файла');
+            }
+
+            const data = await response.json();
+            console.log('Файл успешно загружен:', data);
+        } catch (error) {
+            console.error('Ошибка:', error);
+        }
     };
     return (
         <div >
